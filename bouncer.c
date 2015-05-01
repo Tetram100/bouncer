1fa486cbf54c0bc265ccb6ce711a4b0104d2ad25
tetram <guillaume@dhainaut.fr>
2015-05-01 11:21:25 +0200

    Début de pcap, recupère un packet après filtre icmp et le cast pour avoir accès aux infos. A TESTER

--- bouncer.c
+++ bouncer.c
@@ -6,12 +6,48 @@
 
 
 void process_pkt(u_char *args, const struct pcap_pkthdr *header,
-	    const u_char *packet);
+	const u_char *packet);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-	/* Include here your code to initialize the PCAP capturing process */
+pcap_t *handle;			/* Session handle */
+		char *dev;			/* The device to sniff on */
+		char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+		struct bpf_program fp;		/* The compiled filter */
+		char filter_exp[] = "icmp[icmptype] == icmp-echo and icmp[icmptype] == icmp-echoreply";	/* The filter expression */
+		bpf_u_int32 mask;		/* Our netmask */
+		bpf_u_int32 net;		/* Our IP */
+		struct pcap_pkthdr header;	/* The header that pcap gives us */
+		const u_char *packet;		/* The actual packet */
 
-	/* Initialize raw socket */
+		/* Define the device */
+	dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
+		/* Find the properties for the device */
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
+		/* Open the session in promiscuous mode */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+		/* Compile and apply the filter */
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+	
+	pcap_loop(handle, -1, process_pkt, NULL);
+
 }//End of the bouncer
